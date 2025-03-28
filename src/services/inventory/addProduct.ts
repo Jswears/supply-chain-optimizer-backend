6e8fcd29536baec4372a490c06413660f803c072
Joaquin Swears Salinas <106037859+Jswears@users.noreply.github.com>
2025-03-28 11:51:07 +0100

    Feature/orders service (#18)
    * feat(inventory): implementing logging util to addProduct
    
    * feat(inventory): iac for dyanmoDB inventory table
    
    * feat(inventory): iac for addProduct iam roles & policies
    
    * feat(inventory): iac for addProduct
    
    * feat(inventory): config fix
    
    * fix(utils): esbuildconfig and logger for better structure in cloudwatch
    
    * feat(inventory): getAllProducts function
    
    * feat(inventory): iac for getAllProducts
    
    * feat(inventory): getProduct function
    
    * feat(inventory): iac for getProduct
    
    * fix(inventory): adding warehouse_id to get product correctly
    
    * feat(inventory): iac, templates for: * inventory-api, inventory-stack
    
    * fix(utils): changing schema from name to product_name
    
    * feat(inventory): add getAllProductByWarehouseId function
    
    * fix(inventory iac): fix db, and added GSI-Warehouse
    
    * feat(inventory IaC): iac for getAllProductsByWarehouseId: Roles, Api, Lambda
    
    * fix(inventory): small change from id to productId
    
    * feat(inventory): add deleteProduct function
    
    * feat(inventory IaC): add iac for deleteProduct: Roles, Api, Lambda
    
    * perf(logging): adding error logging to add functions:
    
    * feat(inventory): add updateProduct function
    
    * feat(inventory-iac): iac for updateProduct: Roles, Api, Lambda
    
    * feat(iac): add iac for inventory and orders with db templates, lambdas, and s3 deployment
    
    - implemented infrastructure as code for inventory and orders services
    
    - added separate database templates for dynamodb and rds
    
    - created required lambda functions and iam roles
    
    - set up networking, vpc, and secrets manager integration
    
    - replaced cloudformation package with s3-based upload and deployment workflow
    
    * fix(inventory): fixing return error response to congruent message
    
    * feat(orders): add createOrder lambda function
    
    * feat(orders): add getOrders lambda function
    
    * feat(orders): add initializeDB and updateSecret lambda functions
    
    * feat(orders): add rds-postgres util and validators for orders

--- src/services/inventory/addProduct.ts
+++ src/services/inventory/addProduct.ts
@@ -4,6 +4,7 @@ import { errorResponse, successResponse } from '../../utils/response';
 import { productSchema, validateInput } from '../../utils/validators/inventoryValidation';
 import { dynamoDb, INVENTORY_TABLE_NAME } from '../../utils/dynamodb';
 import { correlationId, StructuredLogger } from '../../utils/logger';
+
 export const handler: APIGatewayProxyHandler = async (event) => {
   const logger = new StructuredLogger({
     correlationId,
